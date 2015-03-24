b6a0cf938f1a8db7ade103d40745534f700768f0
jairo <jairo@jairo-VirtualBox>
2015-03-24 15:42:57 -0400

    modificacion del proyecto de la clase directorio

--- src/com/AdmPlugin/motorLisa/Directorio.java
+++ src/com/AdmPlugin/motorLisa/Directorio.java
@@ -1,14 +1,62 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.AdmPlugin.motorLisa;
 
-/**
- *
- * @author jairo
- */
+import  com.library.util.Empty;
+import java.io.File;
+import java.io.FilenameFilter;
+
 public class Directorio {
     
+    {
+        extencionFiltro = Empty.getString();
+        archivosContados = Empty.getInt();
+    }
+    
+    private String extencionFiltro;
+    private int archivosContados;
+
+    public int getArchivosContados() {
+        return archivosContados;
+    }
+    
+    private void escribirCarpeta(File[] archivos){
+        
+        for(File archivo:archivos){
+            if(archivo.isDirectory()){
+              File[] directorio = archivo.listFiles();
+              escribirCarpeta(directorio);
+            }else{
+                System.out.println(archivo.getAbsolutePath());      
+            }
+        }
+    }
+    
+    public void doBuscarRutas(String ruta){
+        
+        File Directorio = new File(ruta);
+        
+        if(Directorio.exists()){
+         
+            File[] archivos;
+
+            if(!extencionFiltro.isEmpty())
+            {
+                archivos = Directorio.listFiles( new FilenameFilter() {
+
+                    @Override
+                    public boolean accept(File dir, String name) {
+                       return name.endsWith(extencionFiltro);
+                    }
+                });
+            }else{
+                archivos = Directorio.listFiles();
+            }
+            
+            escribirCarpeta(archivos);
+         }  
+    }
+
+    public void setExtencionFiltro(String extencionFiltro) {
+        this.extencionFiltro = extencionFiltro;
+    }
 }
